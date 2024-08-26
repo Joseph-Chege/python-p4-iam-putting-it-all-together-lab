dc7bd51908c0cdfaa2ceca4e19dc3f81bbf1ead2
Joseph-Chege <mjchege23@gmail.com>
2024-08-26 15:37:58 +0300

    Completed the IAM lab

--- server/models.py
+++ server/models.py
@@ -7,9 +7,65 @@ from config import db, bcrypt
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
+    serialize_rules = ('-recipes.user',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String, nullable=True)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+
+    recipes = db.relationship('Recipe', back_populates='user', cascade="all, delete-orphan")
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("Password hash is not accessible.")
+
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+    
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+    
+    @validates('_password_hash')
+    def validate_password_hash(self, key, password_hash):
+        if not password_hash:
+            raise ValueError('Password must not be empty.')
+        return password_hash
+    
+
+    def __repr__(self):
+        return f'User {self.username}, {self._password_hash}, {self.bio}'
+      
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
+
+    serialize_rules = ('-users.recipe',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.Text, nullable=False)
+    minutes_to_complete = db.Column(db.Integer, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship('User', back_populates='recipes')
+
+        
+    @validates('instructions')
+    def validate_instructions(self, key, instructions):
+        if len(instructions) < 50:
+            raise ValueError('Instructions must be at least 50 characters long.')
+        return instructions
     
-    pass
+        
+    def __repr__(self):
+        return f'Recipe {self.title}, {self.instructions}, {self.minutes_to_complete}'
+        
